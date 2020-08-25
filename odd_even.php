0e61ff2abc91bb591577ebca7d4b50dd2050ebea
TysonHawk <mamin182093@gmail.com>
2020-08-25 17:07:30 +0600

    functionality implemented

--- odd_even.php
+++ odd_even.php
@@ -1,3 +1,11 @@
 <?php
 
-$arr = [5 , 9 , 7];
+$arr = [4 , 9 , 7];
+
+for( $i = 0; $i < count($arr); $i++ ) {
+    if( $arr[$i] % 2 == 0 ) {
+        echo 'Even'.PHP_EOL;
+    } else {
+        echo 'Odd'.PHP_EOL;
+    }
+}
