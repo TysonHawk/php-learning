73a41c03991ac896abebf583ef247ab44448c382
TysonHawk <mamin182093@gmail.com>
2020-08-27 18:01:17 +0600

    Even Odd Check

--- odd_even.php
+++ odd_even.php
@@ -1,7 +1,20 @@
 <?php
 
+// Printing odd number from 1 to 20
+
 for( $i = 1 ; $i <= 20 ; $i++ ){
     if( $i % 2 != 0){
         echo $i . PHP_EOL;
     }
-}
+};
+
+// Checking if the number is even or odd from 1 to 10
+
+for( $i = 1 ; $i <= 10 ; $i++ ){
+    if( $i % 2 == 0){
+        echo "Even" . PHP_EOL;
+    }
+    else{
+        echo "Odd" . PHP_EOL;
+    }
+};
